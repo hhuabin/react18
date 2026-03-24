4cb2aa03b8975ef1ec59aed834647f791ffcc3ca
hhuabin <hhuabin@gmail.com>
2026-03-24 17:42:56 +0800

    CSSMotion

--- src/pages/CSSMotion/CSSMotion.tsx
+++ src/pages/CSSMotion/CSSMotion.tsx
@@ -1,16 +1,18 @@
-import { useState, useEffect, memo, forwardRef, type ForwardedRef, useRef } from 'react'
+import { useState, memo, forwardRef } from 'react'
 
 import CSSMotion from '@/components/CSSMotion'
 
 import './CSSMotion.less'
 
 type MemoProps = {
-    className: string;
+    visible?: boolean;
+    className?: string;
+    style?: React.CSSProperties;
 }
 
 // eslint-disable-next-line prefer-arrow-callback
 const ForwardedComponent = forwardRef(function ForwardedComponent(
-    { className: motionClassName }: MemoProps,
+    { className: motionClassName, style: motionStyle }: MemoProps,
     ref: React.Ref<HTMLDivElement>,        // 这个 ref 来自父组件或 CSSMotion，用于访问真实 DOM
 ) {
     // 把 CSSMotion 的 ref 绑定到当前的 HTMLDivElement
@@ -19,21 +21,70 @@ const ForwardedComponent = forwardRef(function ForwardedComponent(
         <div
             ref={ref}
             className={'motion-children' + (motionClassName ? ' ' + motionClassName : '')}
+            style={motionStyle}
         />
     )
 })
+// memo 组件需要配合 forwardRef 组件使用
 const MemoComponent = memo(ForwardedComponent)
 
 const CSSMotionComponents: React.FC = () => {
 
+    const [showAppear, setShowAppear] = useState(true)
+    const [appearVisible, setAppearVisible] = useState(true)
     const [enterVisible, setEnterVisible] = useState(true)
     const [memoVisible, setMemoVisible] = useState(true)
 
-    // 记忆组件
+    const changeShowAppear = () => {
+        if (!showAppear) {
+            setShowAppear(true)
+            setAppearVisible(true)
+        } else {
+            setAppearVisible(false)
+        }
+    }
+    const onVisibleChanged = (visible: boolean) => {
+        setShowAppear(visible)
+    }
 
     return (
         <div className={'css-motion' + ' w-full min-h-full'}>
-            <div className='w-full p-3'>
+            <div className='w-full p-3' key={1}>
+                <div className='flex items-center full'>
+                    <div className=''>appear（首次进入）</div>
+                    <button
+                        type='button'
+                        className='px-[16px] border border-[var(--color-border)] rounded-md m-2 text-[16px] bg-[var(--bg-color)] select-none
+                            text-[var(--color-text)] leading-[32px] hover:border-[var(--color-primary-hover)] hover:text-[var(--color-primary-hover)]'
+                        onClick={() => { changeShowAppear() }}
+                    >
+                        <span>change show appear</span>
+                    </button>
+                </div>
+                <div className='w-full h-[150px] mt-2'>
+                    {
+                        showAppear && (
+                            <CSSMotion
+                                visible={appearVisible}
+                                motionName="fade"
+                                motionAppear={true}
+                                removeOnLeave={true}
+                                onVisibleChanged={onVisibleChanged}
+                            >
+                                {({ className: motionClassName, style: motionStyle }, motionRef) => (
+                                    <div
+                                        ref={motionRef}
+                                        className={'motion-children' + (motionClassName ? ' ' + motionClassName : '')}
+                                        style={motionStyle}
+                                    ></div>
+                                )}
+                            </CSSMotion>
+                        )
+                    }
+                </div>
+            </div>
+
+            <div className='w-full p-3' key={2}>
                 <div className='flex items-center full'>
                     <div className=''>enter（进入）</div>
                     <button
@@ -50,10 +101,11 @@ const CSSMotionComponents: React.FC = () => {
                         visible={enterVisible}
                         motionName="fade"
                         motionAppear={false}
-                        removeOnLeave
+                        removeOnLeave={false}
                     >
                         {({ className: motionClassName, style: motionStyle }, motionRef) => (
                             <div
+                                ref={motionRef}
                                 className={'motion-children' + (motionClassName ? ' ' + motionClassName : '')}
                                 style={motionStyle}
                             ></div>
@@ -62,7 +114,7 @@ const CSSMotionComponents: React.FC = () => {
                 </div>
             </div>
 
-            <div className='w-full p-3'>
+            <div className='w-full p-3' key={3}>
                 <div className='flex items-center full'>
                     <div className=''>Memo记忆组件</div>
                     <button
@@ -79,9 +131,9 @@ const CSSMotionComponents: React.FC = () => {
                         visible={memoVisible}
                         motionName="fade"
                         motionAppear={false}
-                        removeOnLeave
+                        removeOnLeave={false}
                     >
-                        {({ className }) => <MemoComponent className={className!} />}
+                        {({ className, style }) => <MemoComponent className={className} style={style} />}
                     </CSSMotion>
                 </div>
             </div>
